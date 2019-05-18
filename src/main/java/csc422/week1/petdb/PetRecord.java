3c56c82c01dcfe9667a87fe19d71f3825fc8e826
mike <mike@MIKEBOOK.in.mnice.org>
2019-05-18 16:38:30 -0500

    Search for pet by Name or by Age is working.
    Moved functionality and display statements around to various methods to improve workflow.

--- src/main/java/csc422/week1/petdb/PetRecord.java
+++ src/main/java/csc422/week1/petdb/PetRecord.java
@@ -35,7 +35,6 @@ public class PetRecord {
     
     public static void displayChoices(ArrayList<PetRecord> petRecords) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("ljk");
         System.out.println();
         System.out.println("What would you like to do?");
         System.out.println("1) View all pets");
@@ -52,39 +51,111 @@ public class PetRecord {
         launchChoice(choice, petRecords);
     }
     
-    public static void viewPets(ArrayList<PetRecord> petRecords, boolean updating) {
-        Scanner reader = new Scanner(System.in);
+    public static void viewPets(ArrayList<PetRecord> petRecords, int nextStep) {
+        System.out.println();
+        //Header
         String divider = "+----------------------+";
         System.out.println(divider);
         System.out.println("| ID | NAME      | AGE |");
         System.out.println(divider);
+        //List
         for (PetRecord pr : petRecords) {
             System.out.printf("| %2d | %9s | %3d |\n", petRecords.indexOf(pr), pr.getName(), pr.getAge());
         }
+        //Footer
         System.out.println(divider + "\n");
-        if(updating) {
-            System.out.println("Type the pet's name and press ENTER: ");
-            String petName = reader.nextLine();
-            System.out.println("Type the pet's age (as a number) and press ENTER: ");
-            int petAge = reader.nextInt();
-            petRecords.add( new PetRecord(petName, petAge) );
-
-            System.out.println("Please choose one option.");
-            System.out.println("1) Add another pet");
-            System.out.println("0) Return to main menu");
-            int answer = reader.nextInt();
-            if(answer == 1) {
-                viewPets(petRecords, true);
-            } else {
+        System.out.println(petRecords.size() + " rows in set.\n");
+        switch(nextStep) {
+            case 0:
+                addPets(petRecords);
+            case 1:
+                searchPetsByName(petRecords);
+            case 2:
+                searchPetsByAge(petRecords);
+            case 3:
+                updatePet(petRecords);
+            case 4:
+                removePet(petRecords);
+            default:
                 displayChoices(petRecords);
+        }
+    }
+
+    public static void viewPetsFilteredByName(ArrayList<PetRecord> petRecords, String petName) {
+        int filteredResultCount = 0;
+        System.out.println();
+        //Header
+        String divider = "+----------------------+";
+        System.out.println(divider);
+        System.out.println("| ID | NAME      | AGE |");
+        System.out.println(divider);
+        //List
+        for (PetRecord pr : petRecords) {
+            if(pr.getName().equals(petName)) {
+                filteredResultCount ++;
+                System.out.printf("| %2d | %9s | %3d |\n", petRecords.indexOf(pr), pr.getName(), pr.getAge());
             }
+        }
+        //Footer
+        System.out.println(divider + "\n");
+        System.out.println(filteredResultCount + " rows in set.\n");
+        displayChoices(petRecords);
+    }
+
+    public static void viewPetsFilteredByAge(ArrayList<PetRecord> petRecords, int petAge) {
+        int filteredResultCount = 0;
+        System.out.println();
+        //Header
+        String divider = "+----------------------+";
+        System.out.println(divider);
+        System.out.println("| ID | NAME      | AGE |");
+        System.out.println(divider);
+        //List
+        for (PetRecord pr : petRecords) {
+            if(pr.getAge() == petAge) {
+                filteredResultCount ++;
+                System.out.printf("| %2d | %9s | %3d |\n", petRecords.indexOf(pr), pr.getName(), pr.getAge());                
+            }
+        }
+        //Footer
+        System.out.println(divider + "\n");
+        System.out.println(filteredResultCount + " rows in set.\n");
+                displayChoices(petRecords);
+    }
+    
+    public static void addPets(ArrayList<PetRecord> petRecords) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the pet's name and press ENTER: ");
+        String petName = reader.nextLine();
+        System.out.println("Type the pet's age (as a number) and press ENTER: ");
+        int petAge = reader.nextInt();
+        petRecords.add( new PetRecord(petName, petAge) );
+
+        System.out.println("Please choose one option.");
+        System.out.println("1) Add another pet");
+        System.out.println("0) Return to main menu");
+        int answer = reader.nextInt();
+        if(answer == 1) {
+            viewPets(petRecords, 0);
         } else {
             displayChoices(petRecords);
         }
     }
     
-    public static void addPets(ArrayList<PetRecord> petRecords) {
-        viewPets(petRecords, true);
+    public static void searchPetsByName(ArrayList<PetRecord> petRecords) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the pet's name and press ENTER: ");
+        String petName = reader.nextLine();
+        //would be a SELECT STATEMENT in case of real DB
+        viewPetsFilteredByName(petRecords, petName);
+    }
+    
+    public static void searchPetsByAge(ArrayList<PetRecord> petRecords) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the pet's age (as a number) and press ENTER: ");
+        int petAge = reader.nextInt();
+        //would be a SELECT STATEMENT in case of real DB
+        viewPetsFilteredByAge(petRecords, petAge);
     }
     
     public static void updatePet(ArrayList<PetRecord> petRecords) {
@@ -99,22 +170,10 @@ public class PetRecord {
         displayChoices(petRecords);
     }
     
-    public static void searchPetsByName(ArrayList<PetRecord> petRecords) {
-        //TODO: search pets by name functionality
-        System.out.println("This feature coming soon in a future release.");
-        displayChoices(petRecords);
-    }
-    
-    public static void searchPetsByAge(ArrayList<PetRecord> petRecords) {
-        //TODO: search pets by age functionality
-        System.out.println("This feature coming soon in a future release.");
-        displayChoices(petRecords);
-    }
-    
     public static void launchChoice(int choice, ArrayList<PetRecord> petRecords) {
         switch(choice) {
             case 1:
-                viewPets(petRecords, false);
+                viewPets(petRecords, 5);
                 break;
             case 2:
                 addPets(petRecords);
@@ -140,7 +199,8 @@ public class PetRecord {
         ArrayList<PetRecord> petRecords = new ArrayList<PetRecord>();
         petRecords.add( new PetRecord("Spot",10) );
         petRecords.add( new PetRecord("Bear",6) );
-        
+        System.out.println("Pet Database Program.");
+
         displayChoices(petRecords);
     }
 }
